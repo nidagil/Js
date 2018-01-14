1d3c8953991637c0c23ff9cb89f4a075713e6a83
nidagil <panida.gilbert@gmail.com>
2018-01-14 13:03:34 +1300

    add testing

--- src/index.js
+++ src/index.js
@@ -1,18 +1,30 @@
 // var uniqueRandomArray = require('unique-random-array');
-// var dishNames = require('./db.json');
+// var movieNames = require('./data-movie.json');
 
 // module.exports = {
-//   all: dishNames,
-//   random: uniqueRandomArray([1, 2, 3, 4])
-  
+//     all: movieNames,
+//     random: uniqueRandomArray(movieNames)
 // };
+
+//You can write your own function or using third party library.
 
 var uniqueRandomArray = require('unique-random-array');
 var movieNames = require('./data-movie.json');
+var getRandomMovie = uniqueRandomArray(movieNames);
 
 module.exports = {
     all: movieNames,
-    random: uniqueRandomArray(movieNames)
-
+    random: random
 };
 
+function random(number) {
+   if (number === undefined) {
+       return getRandomMovie();
+   } else {
+       var randomMovies = [];
+       for (var i = 0; i < number; i++) {
+           randomMovies.push(getRandomMovie());
+       }
+       return randomMovies;
+   }
+}
